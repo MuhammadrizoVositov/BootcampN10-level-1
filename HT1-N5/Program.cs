97d9fe0cea3ad5f1b5d828687b641137c9250a9b
MuhammadrizoVositov <vositovmuhammadrizo13@gmail.com>
2023-07-17 09:12:47 +0500

    add new homework

---
+++ HT1-N5/Program.cs
@@ -0,0 +1,57 @@
+﻿int yig = 0;
+var test = new string[]
+{
+    "1- Test\n 5*5 jabobi ?",
+    "2- Test\n 6*6 jabobi ?",
+    "3- Test\n 7*7 jabobi ?",
+    "4- Test\n 8*8 jabobi ?",
+    "5- Test\n 9*9 jabobi ?"
+};
+var ToJav = new string[]
+    {
+        "A)25",
+        "A)36",
+        "A)49",
+        "A)64",
+        "A)81",
+    };
+var NoJav = new string[]
+    {
+        "B)32",
+        "B)50",
+        "B)97",
+        "B)12",
+        "B)44",
+    };
+var UserAnswer = new string[5];
+for (int i = 0; i < test.Length; i++)
+{
+    Console.WriteLine(test[i]);
+    Console.WriteLine(ToJav[i]);
+    Console.WriteLine(NoJav[i]);
+    Console.WriteLine("Javobni tanlang:");
+    var ozg = Console.ReadLine();
+    UserAnswer[i] = ozg;
+};
+for (int i = 0;i < test.Length;i++)
+{
+
+    if (UserAnswer[i] == ToJav[i][0].ToString())
+    {
+        Console.WriteLine("Correct ");
+        Console.WriteLine("Ball: 5");
+        yig += 5;
+    }
+    else
+    {
+        Console.WriteLine("Wrong answer");
+        Console.WriteLine("Balls: 0");
+    }
+}
+Console.WriteLine("Your total mark is {0}", yig);
+
+
+
+
+
+
---
+++ N5-HT2/Program.cs
@@ -0,0 +1,36 @@
+﻿var againnst = "w".Contains("e");
+while (againnst==false)
+{
+    Console.WriteLine("Enter first number :");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter second number :");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter operation ");
+    var op = Console.ReadLine();
+
+    switch (op)
+    {
+        case "+":
+            num1 += num2;
+            Console.WriteLine(num1);
+            break;
+        case "-":
+            num1 -= num2;
+            Console.WriteLine(num1);
+            break;
+        case "*":
+            num1 *= num2;
+            Console.WriteLine(num1);
+            break;
+        case "/":
+            num1 /= num2;
+            Console.WriteLine(num1);
+            break;
+        case "e":
+            againnst = true; break;
+        default:
+            Console.WriteLine("Bunday buyruq mavjud emas");
+            break;
+
+    }
+}
